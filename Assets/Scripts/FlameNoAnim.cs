f74ccf5109fe71197ea72218518b1d336ba06e6a
Chris <Stofferdyr@gmail.com>
2020-02-22 16:03:42 +0100

    better terrain. move stuff. health script. slider and more

---
+++ Assets/Scripts/FlameNoAnim.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlameNoAnim : MonoBehaviour
+{
+    [SerializeField] GameObject flameObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        flameObj.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            flameObj.SetActive(true);
+        }
+        if(Input.GetKeyUp(KeyCode.Q))
+        {
+            flameObj.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] public Slider hpBar;
+    [SerializeField] float maxHealth = 100f;
+    [SerializeField] float currentHealth;
+    void Start()
+    {
+
+        currentHealth = maxHealth;
+
+    }
+
+    void Update()
+    {
+        hpBar.value = currentHealth;
+
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            currentHealth -= 10f;
+        }
+    }
+}
