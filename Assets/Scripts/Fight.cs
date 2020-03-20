191ed15c883f1ec924940b0958000cfeacfb4161
Chris <Stofferdyr@gmail.com>
2020-03-20 19:13:17 +0100

    trying animation state things

--- Assets/Scripts/Fight.cs
+++ Assets/Scripts/Fight.cs
@@ -5,80 +5,31 @@ using UnityEngine;
 public class Fight : MonoBehaviour
 {
 
-    [SerializeField] float timeBetweenAttacks = 1f;
-    [SerializeField] float damage = 1f;
+    [SerializeField]  float damage = 1f;
+    [SerializeField] float stabRange = 3f;
     [SerializeField] float stabDamage = 1f;
     [SerializeField] Transform swordAim;
     [SerializeField] Transform stabAim;
     [SerializeField] float offset = 1f;
     [SerializeField] float radius = 1f;
-    [SerializeField] float stabRange = 3f;
     [SerializeField] GameObject bloodEffect;
     public float timeSinceLastAttack = Mathf.Infinity;
-    Rigidbody rb;
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
 
     // Update is called once per frame
     void Update()
     {
-        AttackBehavior();
         Debug.DrawRay(stabAim.transform.position, transform.TransformDirection(Vector3.forward) * stabRange, Color.yellow);
-
     }
-    private void AttackBehavior()
+    public float GetDamage()
     {
-        timeSinceLastAttack += Time.deltaTime;
-        if (timeSinceLastAttack > timeBetweenAttacks && Input.GetKey(KeyCode.Mouse0))
-        {
-            // This will trigger the Hit() event.
-            TriggerAttack();
-            timeSinceLastAttack = 0;
-        }
-        else if (Input.GetKeyUp(KeyCode.Mouse0))
-        {
-            StopAttack();
-        }
-        if (Input.GetKey(KeyCode.Mouse1))
-        {
-            TriggerAttackHeavy();
-        }
-        else if (Input.GetKeyUp(KeyCode.Mouse1))
-        {
-            StopAttackHeavy();
-        }
-        if (Input.GetKey(KeyCode.Z))
-        {
-
-            TriggerStab();
-        }
-        else if (Input.GetKeyUp(KeyCode.Z))
-        {
-            StopStab();
-        }
-        if (Input.GetKey(KeyCode.Mouse0) && Input.GetKey(KeyCode.LeftShift))
-        {
-            TriggerRunJumpAttack();
-        }
-        else if (Input.GetKeyUp(KeyCode.Mouse0))// && Input.GetKeyUp(KeyCode.LeftShift))
-        {
-            StopRunJumpAttack();
-        }
-        if(Input.GetKey(KeyCode.E))
-        {
-            TriggerDance();
-        }
-        if(Input.GetKeyUp(KeyCode.E))
-        {
-            StopDance();
-        }
-
+        return damage;
     }
 
-
+    public void SetDamage(float newDamage)
+    {
+        damage = newDamage;
+    }
+    //Animation event for Light Attack
     void Hit()
     {
         LayerMask layer = LayerMask.GetMask("Enemy");
@@ -88,12 +39,13 @@ public class Fight : MonoBehaviour
             {
                 Vector3 offSet = new Vector3(0, offset, 0);
                 var bloodClone = Instantiate(bloodEffect, c.ClosestPoint(swordAim.transform.position + offSet), swordAim.transform.rotation);
-                c.gameObject.GetComponent<Health>().TakeDamage(damage);
+                c.gameObject.GetComponent<Health>().TakeDamage(GetDamage());
             }
         }
 
     }
 
+    //Animation event for stab
     void Stab()
     {
         LayerMask layer = LayerMask.GetMask("Enemy");
@@ -107,60 +59,13 @@ public class Fight : MonoBehaviour
             var bloodClone = Instantiate(bloodEffect, hit.point, hit.rigidbody.gameObject.transform.rotation);
         }
     }
+
+
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(swordAim.position, radius);
     }
-    private void StopDance()
-    {
-        GetComponent<Animator>().SetBool("dance", false);
-    }
-    private void TriggerDance()
-    {
-        GetComponent<FlameThrowing>().StopFlame();
-        GetComponent<Animator>().SetBool("dance", true);
-    }
-    private void StopAttack()
-    {
-        GetComponent<Animator>().SetBool("lightAttack", false);
-    }
-    private void TriggerAttack()
-    {
-        GetComponent<FlameThrowing>().StopFlame();
-        GetComponent<Animator>().SetBool("lightAttack", true);
-    }
 
-    private void StopAttackHeavy()
-    {
-        GetComponent<Animator>().ResetTrigger("heavyAttack");
-        GetComponent<Animator>().SetTrigger("stopHeavyAttack");
-    }
-    private void TriggerAttackHeavy()
-    {
-        GetComponent<Animator>().ResetTrigger("stopHeavyAttack");
-        GetComponent<Animator>().SetTrigger("heavyAttack");
-    }
-    private void StopStab()
-    {
-        GetComponent<Animator>().ResetTrigger("stab");
-        GetComponent<Animator>().SetTrigger("stopStab");
-    }
-    private void TriggerStab()
-    {
 
-        GetComponent<Animator>().ResetTrigger("stopStab");
-        GetComponent<Animator>().SetTrigger("stab");
-    }
-    private void StopRunJumpAttack()
-    {
-        GetComponent<Animator>().ResetTrigger("runJumpAttack");
-        GetComponent<Animator>().SetTrigger("stopRunJumpAttack");
-    }
-    private void TriggerRunJumpAttack()
-    {
-
-        GetComponent<Animator>().ResetTrigger("stopRunJumpAttack");
-        GetComponent<Animator>().SetTrigger("runJumpAttack");
-    }
 }
